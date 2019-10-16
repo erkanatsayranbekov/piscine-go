16fae726533ec03ff1858cd3a1547956ef51d512
justauser123 <elmira.ali.98@gmail.com>
2019-10-16 23:57:35 +0600

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -4,24 +4,40 @@ import (
 	"github.com/01-edu/z01"
 )
 
-func PrintNbr(n int) {
-	if n <= 0 {
-		z01.PrintRune('-')
-		Help(n * (-1))
+func Help(n int) {
+	if n == 0 {
+		return
 	} else {
-		Help(n)
+		charX := '0'
+		for x := 0; x > n%10; x-- {
+			charX++
+		}
+		Help(n / 10)
+		z01.PrintRune(charX)
 	}
 }
 
-func Help(n int) {
-	div := n % 10
-	var x rune = '0'
-	for i := 1; i <= div; i++ {
-		x++
-	}
-	if div == 0 {
+func HelpForPositive(n int) {
+	if n == 0 {
 		return
+	} else {
+		charX := '0'
+		for x := 0; x < n%10; x++ {
+			charX++
+		}
+		HelpForPositive(n / 10)
+		z01.PrintRune(charX)
+
 	}
-	Help(n / 10)
-	z01.PrintRune(x)
+}
+
+func PrintNbr(n int) {
+	if n < 0 {
+		z01.PrintRune(45)
+		Help(n)
+	} else if n == 0 {
+		z01.PrintRune(48)
+	} else {
+		HelpForPositive(n)
+	}
 }
