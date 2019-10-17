2f5a2f461114c454753f96a3b4f931b83e051921
justauser123 <elmira.ali.98@gmail.com>
2019-10-17 23:42:08 +0600

    basicatoi

---
+++ basicatoi.go
@@ -0,0 +1,33 @@
+package piscine
+
+func StrLen(str string) int {
+	a := 0
+	s := []rune(str)
+	for i := range s {
+		a = i
+	}
+	return a
+}
+
+var m = map[rune]int{
+	'0': 0,
+	'1': 1,
+	'2': 2,
+	'3': 3,
+	'4': 4,
+	'5': 5,
+	'6': 6,
+	'7': 7,
+	'8': 8,
+	'9': 9,
+}
+
+func BasicAtoi(s string) int {
+	var di int
+	for _, digit := range s {
+		if d, ok := m[digit]; ok {
+			di = (di * 10) + d
+		}
+	}
+	return di
+}
