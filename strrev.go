1580f858d9296d0e16a102fc00e5b7e730d626c7
justauser123 <elmira.ali.98@gmail.com>
2019-10-17 22:45:46 +0600

    strrev

---
+++ strrev.go
@@ -0,0 +1,20 @@
+package piscine
+
+func StrLen(str string) int {
+	a := 0
+	s := []rune(str)
+	for i := range s {
+		a = i
+	}
+	return a
+}
+
+func StrRev(s string) {
+	j := 0
+	nrune := []rune(s)
+	for i := StrLen(s); i >= 0; i-- {
+		nrune[j] = rune(s[i])
+		j++
+	}
+	return string(nrune)
+}
