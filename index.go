b1450976abe3d9433c0c947628e8d28c6c26b202
justauser123 <elmira.ali.98@gmail.com>
2019-10-22 23:31:27 +0600

    index.go

--- index.go
+++ index.go
@@ -10,24 +10,24 @@ func Len(s string) int {
 
 func Index(s string, toFind string) int {
 	result := -1
-	StrLen := Len(s)
-	FinLen := Len(toFind)
+	lenS := Len(s)
+	lenF := Len(toFind)
 	arrS := []rune(s)
 	arrF := []rune(toFind)
 	if s == "" {
 		return 0
 	}
 	for index, letter := range s {
-		if letter == arrF[0] && index+FinLen <= StrLen {
+		if letter == arrF[0] && index+lenF <= lenS {
 			j := 0
-			c := 0
-			for i := index; i < FinLen; i++ {
+			count := 0
+			for i := index; i < lenF; i++ {
 				if arrS[i] == arrF[j] {
-					c++
+					count++
 				}
 				j++
 			}
-			if c == j {
+			if count == j {
 				result = index
 				break
 			}
