c679b0226d3b33f7e5f1724fd72a3024c91eec4c
justauser123 <elmira.ali.98@gmail.com>
2019-10-23 21:49:24 +0600

    index.go

--- index.go
+++ index.go
@@ -1,28 +1,34 @@
 package piscine
 
-// import "fmt"
-
-func Index(s string, toFind string) int {
-	var rune rune
-	for _, i := range toFind {
-		rune = i
-		break
+func Len(table []rune) int {
+	len := 0
+	for range table {
+		len++
 	}
-
-	index := 0
-	for j, i := range s {
-		if i == rune {
-			index = j
-			break
-		} else {
-			index = -1
-		}
-	}
-	return index
+	return len
 }
 
-// func main() {
-// 	fmt.Println(Index("Hello!", "l"))
-// 	fmt.Println(Index("Salut!", "alu"))
-// 	fmt.Println(Index("Ola!", "hO"))
-// }
+func Index(s string, toFind string) int {
+	as := []rune(s)
+	af := []rune(toFind)
+	ls := Len(as)
+	lf := Len(af)
+	index := -1
+	count := 0
+	if lf > 0 {
+		for j, i := range as {
+			if i == af[0] && j+lf <= ls && count == 0 {
+				for k := j; k <= j+lf; k++ {
+					for m := 0; m <= lf-1; m++ {
+						if as[k] == af[m] {
+							index = j
+							count++
+						}
+					}
+				}
+			}
+		}
+		return index
+	}
+	return 0
+}
