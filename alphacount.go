0aaa7745401e9373cc5f4071bda531343e541c36
justauser123 <elmira.ali.98@gmail.com>
2019-10-24 20:24:06 +0600

    q7, t5

--- alphacount.go
+++ alphacount.go
@@ -2,8 +2,8 @@ package piscine
 
 func AlphaCount(str string) int {
 	result := 0
-	for _, element := range str {
-		if element >= 'A' && element <= 'Z' || element >= 'a' && element <= 'z' {
+	for _, i := range str {
+		if i >= 'a' && i <= 'z' || i >= 'A' && i <= 'z' {
 			result++
 		}
 	}
