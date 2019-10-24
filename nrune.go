d9a6b9f1326ff4dfbbb6770fba8fafc9f371328c
justauser123 <elmira.ali.98@gmail.com>
2019-10-24 23:00:05 +0600

    q5, t5

--- nrune.go
+++ nrune.go
@@ -1,13 +1,6 @@
 package piscine
 
 func NRune(s string, n int) rune {
-	l := 0
-	for range s {
-		l++
-	}
-	if n <= l && n > 0 && l > 0 {
-		rune := []rune(s)
-		return rune[n-1]
-	}
-	return '\x00'
+	rune := []rune(s)
+	return rune[n-1]
 }
