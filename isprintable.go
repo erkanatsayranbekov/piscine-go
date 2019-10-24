4b5f2bd6cca568bd9b710afd77c057ed756acd91
justauser123 <elmira.ali.98@gmail.com>
2019-10-24 23:57:07 +0600

    q5, t16

--- isprintable.go
+++ isprintable.go
@@ -1,10 +1,13 @@
 package piscine
 
 func IsPrintable(str string) bool {
-	for _, value := range str {
-		if value < 32 {
+	var result bool
+	for _, j := range str {
+		if j >= 'a' && j <= 'z' || j >= 'A' && j <= 'Z' {
+			result = true
+		} else {
 			return false
 		}
 	}
-	return true
+	return result
 }
