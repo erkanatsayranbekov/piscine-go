e69b7fcfd519f97d880f458683b524c768565b07
justauser123 <elmira.ali.98@gmail.com>
2019-10-24 22:37:57 +0600

    q5, t3

--- trimatoi.go
+++ trimatoi.go
@@ -1,28 +1,14 @@
 package piscine
 
 func TrimAtoi(s string) int {
-	array := []rune{}
-	result := 0
 	sign := 1
-	error := 0
+	result := 0
 	for _, i := range s {
-		if i == '-' && error == 0 {
+		if i >= '0' && i <= '9' {
+			result = result*10 + int(i-48)
+		} else if i == '-' && result == 0 {
 			sign = -1
-		} else if i >= '0' && i <= '9' {
-			array = append(array, i)
-			error++
-		} else {
-			continue
 		}
-	}
-
-	for _, i := range array {
-		num := 0
-		for j := '1'; j <= i; j++ {
-
-			num++
-		}
-		result = result*10 + num
 	}
 	return result * sign
 }
